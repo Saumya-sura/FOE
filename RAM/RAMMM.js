cfa4cc06843db9c8d65331bc70c8d5857e1e6d02
Jainam Shah <shahjainam1617@gmail.com>
2025-01-17 16:58:41 +0530

    ENLARGED

---
+++ RAM/RAMMM.js
@@ -0,0 +1,37 @@
+       
+        function toggleDrawer() {
+            const drawer = document.getElementById('drawer');
+            drawer.classList.toggle('open');
+        }
+        function Enlarge41()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR41.html";
+        }
+        function Enlarge42()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR42.html";
+        }
+        function Enlarge43()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR43.html";
+        }
+        function Enlarge44()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR44.html";
+        }
+        function Enlarge51()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR51.html";
+        }
+        function Enlarge52()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR52.html";
+        }
+        function Enlarge53()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG-DDR54.html";
+        }
+        function Enlarge54()
+        {
+            window.location.href="http://127.0.0.1:5500/RAM/BIG_DDR53.html";
+        }
