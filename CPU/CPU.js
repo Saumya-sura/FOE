6320f13dc873b383619b8b4a28ecde46a651f9b8
Jainam Shah <shahjainam1617@gmail.com>
2025-01-18 14:22:25 +0530

    CPU DONE

---
+++ CPU/CPU.js
@@ -0,0 +1,36 @@
+function toggleDrawer() {
+    const drawer = document.getElementById('drawer');
+    drawer.classList.toggle('open');
+}
+function I1()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I1.html";
+}
+function I2()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I2.html";
+}
+function I3()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I3.html";
+}
+function I4()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I4.html";
+}
+function I5()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I5.html";
+}
+function I6()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I6.html";
+}
+function I7()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I7.html";
+}
+function I8()
+{
+    window.location.href="http://127.0.0.1:5500/CPU/I8.html";
+}
