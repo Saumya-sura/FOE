0a910fca50ddee1d683c299846f615a3b1ae6cb0
Jainam Shah <shahjainam1617@gmail.com>
2025-01-18 11:38:05 +0530

    Homepage Done

---
+++ homepage/homepage.js
@@ -0,0 +1,37 @@
+       
+        function toggleDrawer() {
+            const drawer = document.getElementById('drawer');
+            drawer.classList.toggle('open');
+        }
+        function EnlargeH1()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H1.html";
+        }
+        function EnlargeH2()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H2.html";
+        }
+        function EnlargeH3()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H3.html";
+        }
+        function EnlargeH4()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H4.html";
+        }
+        function EnlargeH5()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H5.html";
+        }
+        function EnlargeH6()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H6.html";
+        }
+        function EnlargeH7()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H7.html";
+        }
+        function EnlargeH8()
+        {
+            window.location.href="http://127.0.0.1:5500/homepage/H8.html";
+        }
